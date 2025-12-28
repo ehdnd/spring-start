c15b4708df11012dc25f3defe51dea2c2407104d
ehdnd_mac <taeungjang@naver.com>
2025-12-28 18:37:37 +0900

    4.21 : 순수 JDBC

--- src/main/java/ehdnd/spring_start/SpringConfig.java
+++ src/main/java/ehdnd/spring_start/SpringConfig.java
@@ -1,13 +1,23 @@
 package ehdnd.spring_start;
 
+import ehdnd.spring_start.repository.JdbcMemberRepository;
 import ehdnd.spring_start.repository.MemberRepository;
 import ehdnd.spring_start.repository.MemoryMemberRepository;
 import ehdnd.spring_start.service.MemberService;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+  private final DataSource dataSource;
+
+  @Autowired
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
   @Bean
   public MemberService memberService() {
@@ -16,7 +26,8 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+//    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 
 }
